bb2021b2f49463338353eb6f94732cdc72a8f28b
Niklas Tasler <niklastasler@gmail.com>
2026-01-25 08:09:36 +0100

    feat: add trip stack

---
+++ modules/trip/default.nix
@@ -0,0 +1,146 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  name = "trip";
+
+  cfg = config.nps.stacks.${name};
+  storage = "${config.nps.storageBaseDir}/${name}";
+
+  category = "General";
+  description = "Trip Planner";
+  displayName = "Trip";
+in {
+  imports = import ../mkAliases.nix config lib name [
+    name
+  ];
+
+  options.nps.stacks.${name} = {
+    enable = lib.mkEnableOption name;
+    extraEnv = lib.mkOption {
+      type = (import ../types.nix lib).extraEnv;
+      default = {};
+      description = ''
+        Extra environment variables to set for the container.
+        Can be used to pass secrets such as the `TMDB_ACCESS_TOKEN`.
+
+        See <https://itskovacs.github.io/trip/docs/getting-started/configuration>
+      '';
+      example = {
+        SOME_SECRET = {
+          fromFile = "/run/secrets/tmdb_access_token";
+        };
+        FOO = "bar";
+      };
+    };
+    oidc = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+        description = ''
+          Whether to enable OIDC login with Authelia. This will register an OIDC client in Authelia
+          and setup the necessary configuration.
+
+          For details, see:
+          - <https://github.com/fccview/jotty/blob/main/howto/SSO.md>
+        '';
+      };
+      clientSecretFile = (import ../authelia/options.nix lib).clientSecretFile;
+      clientSecretHash = (import ../authelia/options.nix lib).derivableClientSecretHash cfg.oidc.clientSecretFile;
+      adminGroup = lib.mkOption {
+        type = lib.types.str;
+        default = "${name}_admin";
+        description = ''
+          Users of this group will be admin
+        '';
+      };
+      userGroup = lib.mkOption {
+        type = lib.types.str;
+        default = "${name}_user";
+        description = ''
+          Users of this group will be able to log in
+        '';
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    nps.stacks.lldap.bootstrap.groups = lib.mkIf cfg.oidc.enable {
+      ${cfg.oidc.userGroup} = {};
+      ${cfg.oidc.adminGroup} = {};
+    };
+    nps.stacks.authelia = lib.mkIf cfg.oidc.enable {
+      oidc.clients.${name} = {
+        client_name = displayName;
+        client_secret = cfg.oidc.clientSecretHash;
+        public = false;
+        authorization_policy = name;
+        require_pkce = false;
+        pkce_challenge_method = "";
+        pre_configured_consent_duration = config.nps.stacks.authelia.oidc.defaultConsentDuration;
+        redirect_uris = [
+          "${cfg.containers.${name}.traefik.serviceUrl}/auth"
+        ];
+        claims_policy = name;
+      };
+
+      settings.identity_providers.oidc.claims_policies.${name}.id_token = [
+        "email"
+        "email_verified"
+        "preferred_username"
+        "name"
+      ];
+
+      settings.identity_providers.oidc.authorization_policies.${name} = {
+        default_policy = "deny";
+        rules = [
+          {
+            policy = config.nps.stacks.authelia.defaultAllowPolicy;
+            subject = [
+              "group:${cfg.oidc.adminGroup}"
+              "group:${cfg.oidc.userGroup}"
+            ];
+          }
+        ];
+      };
+    };
+
+    services.podman.containers = {
+      ${name} = {
+        image = "ghcr.io/itskovacs/trip:1.16.0";
+        exec = "fastapi run /app/trip/main.py --host 0.0.0.0";
+        volumes = [
+          "${storage}/storage:/app/storage"
+        ];
+
+        extraEnv =
+          {
+            OIDC_DISCOVERY_URL = "${config.nps.containers.authelia.traefik.serviceUrl}/.well-known/openid-configuration";
+            OIDC_CLIENT_ID = name;
+            OIDC_CLIENT_SECRET.fromFile = cfg.oidc.clientSecretFile;
+            OIDC_REDIRECT_URI = "${cfg.containers.${name}.traefik.serviceUrl}/auth";
+          }
+          // cfg.extraEnv;
+
+        stack = name;
+        port = 8000;
+        traefik.name = name;
+        homepage = {
+          inherit category;
+          name = displayName;
+          settings = {
+            inherit description;
+            icon = "sh-trip";
+          };
+        };
+        glance = {
+          inherit category description;
+          name = displayName;
+          id = name;
+          icon = "sh:trip";
+        };
+      };
+    };
+  };
+}
