7b040b702ac17a3820bbb02b20a02f18cca0bfcd
Niklas Tasler <niklastasler@gmail.com>
2026-02-08 14:22:08 +0100

    refactor: enable traefik healthcheck

--- modules/traefik/default.nix
+++ modules/traefik/default.nix
@@ -317,6 +317,15 @@ in {
 
       wantsContainer = lib.optional cfg.crowdsec.middleware.enable "crowdsec";
 
+      extraConfig.Container = {
+        Notify = "healthy";
+        HealthCmd = "traefik healthcheck --ping";
+        HealthInterval = "10s";
+        HealthTimeout = "10s";
+        HealthRetries = 5;
+        HealthStartPeriod = "10s";
+      };
+
       # Traefik should only be in a single network and not be added to others by integations (e.g. socket-proxy)
       # Otherwise we lose the ability to assign static ip (only works with single bridge network)
       network = lib.mkForce cfg.network.name;
